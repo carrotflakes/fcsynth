3a95d416faa8f21f322abae5f25d1274e8222f51
carrotflakes <carrotflakes@gmail.com>
2018-08-26 01:30:49 +0900

    implement visual analizer

--- playground/js/main.js
+++ playground/js/main.js
@@ -10,6 +10,7 @@ function buildSynth(model, params) {
     c2: 0,
     mod: 0,
     pitch: 0,
+    spb: 60 / 120,
   };
   return sb.build(model, masterGain, params);
 }
@@ -45,6 +46,7 @@ m1=gain(lv(c1 * 5000))<-sin(fr(f*(2*c2))),
 m2=gain(lv(mod*20))<-sin(fr(5)),
 gain(adsr(velocity,10,100,0.5,100))<-sin(fr(f)<-(m1+m2))
 `.trim(),
+    analyserMode: 0,
   },
   methods: {
     connect() {
@@ -104,12 +106,66 @@ gain(adsr(velocity,10,100,0.5,100))<-sin(fr(f)<-(m1+m2))
     applyModel() {
       synth = buildSynth(fcsynth.source2model(this.model), synth.trackParams);
     },
+    toggleMode() {
+      this.analyserMode = (this.analyserMode + 1) % 3;
+    },
   },
   async mounted() {
     this.midiAccess = await navigator.requestMIDIAccess();
     this.midiDevices = [...this.midiAccess.inputs];
     if (this.midiDevices.length > 0) {
       this.selectedMidiDevice = this.midiDevices[0][1];
+    }
+
+    {
+      const analyser = ac.createAnalyser();
+      masterGain.connect(analyser);
+      analyser.fftSize = 1024;
+      analyser.smoothingTimeConstant = 0.3;
+      const data = new Uint8Array(256);
+      const canvasEl = document.getElementById('canvas');
+      const ctx = canvasEl.getContext('2d');
+
+      const drawCanvas = () => {
+        switch (this.analyserMode) {
+          case 0:
+          case 2:
+            analyser.getByteFrequencyData(data);
+            break;
+          case 1:
+            analyser.getByteTimeDomainData(data);
+            break;
+        }
+        switch (this.analyserMode) {
+          case 0:
+            ctx.fillStyle = 'rgb(255,255,255)';
+            ctx.fillRect(0, 0, 256, 128);
+            ctx.fillStyle = 'rgb(0,0,255)';
+            for (let i = 0; i < 256; ++i) {
+              ctx.fillRect(i, 128 - data[i] / 2, 1, 128);
+            }
+            break;
+          case 1:
+            ctx.fillStyle = 'rgb(255,255,255)';
+            ctx.fillRect(0, 0, 256, 128);
+            ctx.strokeStyle = 'rgb(0,0,255)';
+            ctx.beginPath();
+            ctx.moveTo(0, data[0] / 2);
+            for (let i = 1; i < 256; ++i) {
+              ctx.lineTo(i, data[i] / 2);
+            }
+            ctx.stroke();
+            break;
+          case 2:
+            ctx.drawImage(canvasEl, -1, 0);
+            for (let i = 0; i < 128; ++i) {
+              ctx.fillStyle = `rgb(${255-data[255-i*2]},${255-data[255-i*2]},255)`;
+              ctx.fillRect(255, i, 1, 1);
+            }
+        }
+      }
+
+      setInterval(drawCanvas, 1000 / 30);
     }
   }
 });
